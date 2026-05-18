c54c5327d8d86b483883ba802f522d4e64bc8305
Federico Combi <fedecombi2000@gmail.com>
2026-05-18 17:39:29 +0200

    first exercises
    exercises on the basics of python

---
+++ esercizi/esercizio1.py
@@ -0,0 +1,7 @@
+#creare un programma che specifica 3 dati nome, cognome, eta e che stampi a video tutte e tre
+
+nome = 'Alice'
+cognome = 'Rossi'
+eta = 20
+
+print('nome:',nome,' cognome:',cognome,' eta:',eta)
---
+++ esercizi/esercizio2.py
@@ -0,0 +1,15 @@
+#dati in input 2 numeri dai il risultato delle operazioni matematiche base tra i due
+
+numero1 = input("Inserisci il primo numero: ")
+numero1 = int(numero1)
+numero2 = input("Inserisci il secondo numero: ")
+numero2 = int(numero2)
+
+risultato = numero2 + numero1
+print("somma:",risultato)
+risultato = numero1*numero2
+print("moltiplicazione:",risultato)
+risultato = numero1-numero2
+print("sottrazione:",risultato)
+risultato = numero1/numero2
+print("divisione:",risultato)
