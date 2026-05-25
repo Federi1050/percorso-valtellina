ea0c3d528d530763089d2911bb8400c47a3cb744
Federico Combi <fedecombi2000@gmail.com>
2026-05-25 12:40:23 +0200

    esercizio 16
    creazione di un programma che preleva api e la rende disponibile su rete locale

---
+++ esercizi/esercizio16/main.py
@@ -0,0 +1,23 @@
+import requests
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'Hello World!'
+
+@app.route('/api')
+def api_giver():
+    ris = richiesta_api_fox()
+    return jsonify(ris)
+
+def richiesta_api_fox():
+    resp = requests.get("https://randomfox.ca/floof/", timeout=5)
+    if resp.status_code == 200:
+        return resp.json()
+    else:
+        return {}
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
