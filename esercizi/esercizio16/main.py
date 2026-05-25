2681839b11fd6582f5827a76ea27fdc057ff8edc
Federico Combi <fedecombi2000@gmail.com>
2026-05-25 14:28:57 +0200

    Update main.py

--- esercizi/esercizio16/main.py
+++ esercizi/esercizio16/main.py
@@ -17,7 +17,7 @@ def richiesta_api_fox():
     if resp.status_code == 200:
         return resp.json()
     else:
-        return {}
+        return {"Error":resp.status_code}
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
